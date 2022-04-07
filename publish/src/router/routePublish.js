f15a873d1994e8bfc5b4d55a74ef4c8279a9712a
kimtaeyoon <90013740+mangtaeeee@users.noreply.github.com>
2022-04-07 13:50:34 +0900

    Update routePublish.js

--- publish/src/router/routePublish.js
+++ publish/src/router/routePublish.js
@@ -3,11 +3,7 @@ export default [
         //dashboard
         path: "dashboard",
         component: () => import("@/pages/dashboard/MainDashBoard.vue")
-    },
-    {
-        path:"list",
-        component: () => import("@/pages/dashboard/List.vue")
-    },
+    }
     
 
 ];
