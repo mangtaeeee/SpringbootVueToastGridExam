040563445f0f783f6e5352f93ff07af9d708a8ad
kimtaeyoon <90013740+mangtaeeee@users.noreply.github.com>
2022-05-20 13:17:33 +0900

    Update BoardDAO.java

--- src/main/java/com/test/groupware/board/mapper/BoardDAO.java
+++ src/main/java/com/test/groupware/board/mapper/BoardDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 import com.reffics.groupware.board.model.BoardVO;
 
 
-//@Mapper 마이바티스로 매퍼 선언 해서 안써도됨
+
 public interface BoardDAO {
 	public List<BoardVO> selectEmply(BoardVO bvo);
 	public int insertEmply(BoardVO bvo);
